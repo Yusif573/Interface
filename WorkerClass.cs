9d15a4da4dd6414ff94df987a0dd8c411b5e1ad3
Yusif573 <160158005+Yusif573@users.noreply.github.com>
2024-03-07 22:18:30 +0400

    Add files via upload

---
+++ WorkerClass.cs
@@ -0,0 +1,46 @@
+﻿using EveryHW.Person;
+
+namespace EveryHW.Worker;
+
+public abstract class WorkerClass : PersonClass
+{
+    static public int _staticId { get; set; }
+    public int _id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    private uint? _age;
+    public uint? Age
+    {
+        get { return _age; }
+        set
+        {
+            if (value is uint)
+            {
+                _age = value;
+            }
+            else
+            {
+                Console.WriteLine("Must be a number!");
+                Console.WriteLine("Enter age:");
+                Age = Convert.ToUInt32(Console.ReadLine());
+            }
+        }
+    }
+    public string Position {  get; set; }
+    public int Salary {  get; set; }
+    protected WorkerClass()
+    {
+        _id = _staticId++;
+    }
+    protected WorkerClass(string name, string surname, uint age, string position, int salary)
+        : base(name, surname, age)
+    {
+        _id = _staticId++;
+        Position = position;
+        Salary = salary;
+    }
+    public override string ToString()
+    {
+        return base.ToString()+$"\nPosition:{Position}\nSalary:{Salary}";
+    }
+}
