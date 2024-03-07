1f172847c9a0f91e12ba3add695758350d743faf
Yusif573 <160158005+Yusif573@users.noreply.github.com>
2024-03-07 22:17:00 +0400

    Add files via upload

---
+++ InterfaceProgram.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Teze kecdiyimiz derslerden numuneleri yazdim digerlerini ise dediz ki bos biraxin yeni icinde hec bir kod olmasin ona gore yazmadim uzagi 10a qeder dusurdun de)
